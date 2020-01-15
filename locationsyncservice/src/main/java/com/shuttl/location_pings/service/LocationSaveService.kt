f7bc30d15644aa42db08f4988ccaa467c8bec90b
kshama <kshama.singh@shuttl.com>
2020-01-15 11:18:52 +0530

    Add mock location in LocationSaveService

--- locationsyncservice/src/main/java/com/shuttl/location_pings/service/LocationSaveService.kt
+++ locationsyncservice/src/main/java/com/shuttl/location_pings/service/LocationSaveService.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -11,21 +12,28 @@ import android.os.Binder
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.shuttl.location_pings.mockLocation.MockLocationProviderManager
 import com.shuttl.location_pings.config.components.LocationConfigs
 import com.shuttl.location_pings.config.components.LocationsDB
 import com.shuttl.location_pings.custom.notification
 import com.shuttl.location_pings.data.model.entity.GPSLocation
 import com.shuttl.location_pings.data.repo.LocationRepo
+import com.shuttl.location_pings.mockLocation.MockLocationProvider
+import com.shuttl.locations_sync.BuildConfig
 
 class LocationSaveService : Service() {
+
 
     private val locManager by lazy { applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager }
     private var configs: LocationConfigs = LocationConfigs()
     private val repo by lazy { LocationRepo(LocationsDB.create(applicationContext)?.locationsDao()) }
     private val locListener by lazy {
+
         object : LocationListener {
 
             override fun onLocationChanged(location: Location?) {
+
                 saveLocation(location)
             }
 
@@ -48,6 +56,8 @@ class LocationSaveService : Service() {
     override fun onCreate() {
         startForeground(1, notification(this, "Updating trip details.."))
         work()
+
+
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -61,15 +71,44 @@ class LocationSaveService : Service() {
 
     @SuppressLint("MissingPermission")
     private fun work() {
-        try {
-            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, configs.minTimeInterval.toLong(), configs.minDistanceInterval.toFloat(), locListener, null)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("LocationSave", "GPS can't be accessed. Asked for permission?")
+        if (BuildConfig.BUILD_TYPE.equals("debug")) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+
+                var mMockLocationProvider: MockLocationProvider =
+                    MockLocationProvider(applicationContext)
+                mMockLocationProvider.addMockLocationProvider(
+                    applicationContext.getSystemService(
+                        Context.LOCATION_SERVICE
+                    ) as LocationManager, applicationContext, locListener
+                )
+            }
+        }
+        else {
+            try {
+                locManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    configs.minTimeInterval.toLong(),
+                    configs.minDistanceInterval.toFloat(),
+                    locListener,
+                    null
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.e("LocationSave", "GPS can't be accessed. Asked for permission?")
+            }
         }
     }
 
-    private fun saveLocation(location: Location?) {
+    public fun saveLocation(location: Location?) {
+        if (location != null) {
+            Log.d("LocationSave ", " latitude = " + location.getLatitude())
+            Log.d("LocationSave ", " lng = " + location.getLongitude())
+        }
         repo.addLocation(GPSLocation.create(location))
     }
 
