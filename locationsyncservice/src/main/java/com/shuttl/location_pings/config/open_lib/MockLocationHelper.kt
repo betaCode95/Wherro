c20439b9e4a9017a634d1cc7100f69aa1fc5b8d9
kshama <kshama.singh@shuttl.com>
2020-01-17 14:02:15 +0530

    Add logs in MockLocationHelper

--- locationsyncservice/src/main/java/com/shuttl/location_pings/config/open_lib/MockLocationHelper.kt
+++ locationsyncservice/src/main/java/com/shuttl/location_pings/config/open_lib/MockLocationHelper.kt
@@ -5,28 +5,32 @@ import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
+import android.util.Log
 import com.shuttl.location_pings.service.MockLocationSaveService
 
 object MockLocationHelper {
 
+    private val TAG: String = MockLocationHelper::class.java.simpleName
     private val serviceConnection by lazy {
         object : ServiceConnection {
             override fun onServiceDisconnected(name: ComponentName?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Log.d(TAG, "onServiceDisconnected : Mock location service")
             }
 
             override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Log.d(TAG, "onServiceConnected : Mock location service")
             }
         }
     }
 
     fun startMockLocationService(app: Application) {
+        Log.d(TAG, "startMockLocationService : Start mock location service")
         val mockIntent = Intent(app, MockLocationSaveService::class.java)
         app.startService(mockIntent)
     }
 
     fun stopMockLocationService(app: Application){
+        Log.d(TAG, "stopMockLocationService : Stop mock location service")
         val mockIntent = Intent(app, MockLocationSaveService::class.java)
         app.stopService(mockIntent)
     }
