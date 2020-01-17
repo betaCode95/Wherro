088c9dbaac0705561f308673f65781400db78ce3
kshama <kshama.singh@shuttl.com>
2020-01-17 14:58:21 +0530

    Remove mock location start and stop service

--- app/src/main/java/com/shuttl/packagetest/MainActivity.kt
+++ app/src/main/java/com/shuttl/packagetest/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shuttl.location_pings.config.components.LocationConfigs
 import com.shuttl.location_pings.config.open_lib.LocationsHelper
-import com.shuttl.location_pings.config.open_lib.MockLocationHelper
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (BuildConfig.BUILD_TYPE.equals("debug")) {
-            MockLocationHelper.startMockLocationService(app = application)
-        } else {
-            LocationsHelper.initLocationsModule(app = application, locationConfigs = LocationConfigs(syncUrl = "https://gps.shuttlstage.com/streams/driver/record"))
-        }
+        LocationsHelper.initLocationsModule(app = application, locationConfigs = LocationConfigs(syncUrl = "https://gps.shuttlstage.com/streams/driver/record"))
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (BuildConfig.BUILD_TYPE.equals("debug")) {
-            MockLocationHelper.stopMockLocationService(application)
-        } else {
-            LocationsHelper.stop(application)
-        }
+        LocationsHelper.stop(application)
     }
 }
