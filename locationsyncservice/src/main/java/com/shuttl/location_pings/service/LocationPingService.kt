b34c970701e5ffb1ba4fef3091fdc0d1de7aad79
Nikhil Roy <nikhil.roy@shuttl.com>
2020-01-20 14:10:57 +0530

    [WIP] updated LocationPingService

--- locationsyncservice/src/main/java/com/shuttl/location_pings/service/LocationPingService.kt
+++ locationsyncservice/src/main/java/com/shuttl/location_pings/service/LocationPingService.kt
@@ -3,31 +3,40 @@ package com.shuttl.location_pings.service
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.CountDownTimer
 import android.os.IBinder
 import android.util.Log
 import com.shuttl.location_pings.config.components.LocationConfigs
 import com.shuttl.location_pings.config.components.LocationsDB
 import com.shuttl.location_pings.custom.notification
 import com.shuttl.location_pings.data.repo.LocationRepo
-import java.sql.Time
-import java.util.*
-
 
 class LocationPingService : Service() {
 
     private var configs: LocationConfigs = LocationConfigs()
 
-    private val timer by lazy { Timer() }
-    private val timerTask by lazy {
-        object : TimerTask() {
-            override fun run() {
-                try {
-                    LocationRepo(LocationsDB.create(applicationContext)?.locationsDao()).syncLocations(configs.xApiKey
-                            ?: "", configs.syncUrl ?: "")
-                } catch (e: Exception) {
-                    Log.d("LocationsHelper", e.toString())
-                }
+    private val timer by lazy {
+        object :
+            CountDownTimer(configs.timeout.toLong() * 1000, configs.minSyncInterval.toLong()) {
+            override fun onTick(millisUntilFinished: Long) {
+                pingLocations()
             }
+
+            override fun onFinish() {
+                stopSelf()
+            }
+        }
+    }
+
+    private fun pingLocations() {
+        try {
+            LocationRepo(LocationsDB.create(applicationContext)?.locationsDao()).syncLocations(
+                configs.xApiKey
+                    ?: "", configs.syncUrl ?: "",
+                configs.batchSize
+            )
+        } catch (e: Exception) {
+            Log.d("LocationsHelper", e.toString())
         }
     }
 
@@ -44,7 +53,6 @@ class LocationPingService : Service() {
         super.onDestroy()
         try {
             timer.cancel()
-            timerTask.cancel()
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -58,7 +66,7 @@ class LocationPingService : Service() {
 
     private fun work() {
         try {
-            timer.schedule(timerTask, 0, configs.minSyncInterval.toLong())
+            timer.start()
         } catch (e: Exception) {
             e.printStackTrace()
         }
