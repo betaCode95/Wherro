2f8ecf2d96aafdbc480a95827660a81a36cae976
kshama <kshama.singh@shuttl.com>
2020-01-20 13:59:35 +0530

    Add init module method in MockLocationHelper

--- locationsyncservice/src/main/java/com/shuttl/location_pings/config/open_lib/MockLocationHelper.kt
+++ locationsyncservice/src/main/java/com/shuttl/location_pings/config/open_lib/MockLocationHelper.kt
@@ -2,11 +2,14 @@ package com.shuttl.location_pings.config.open_lib
 
 import android.app.Application
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
 import android.util.Log
 import com.shuttl.location_pings.service.MockLocationSaveService
+import com.shuttl.location_pings.config.components.LocationConfigs
+
 
 object MockLocationHelper {
 
@@ -23,15 +26,25 @@ object MockLocationHelper {
         }
     }
 
+    fun initMockLocationsModule(
+        app: Application,
+        locationConfigs: LocationConfigs
+    ) {
+        Log.d(TAG, "initMockLocationsModule : init mock location module")
+        val saveMockLocationIntent = Intent(app, MockLocationSaveService::class.java)
+        saveMockLocationIntent.putExtra("config", locationConfigs)
+        app.bindService(saveMockLocationIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
     fun startMockLocationService(app: Application) {
         Log.d(TAG, "startMockLocationService : Start mock location service")
-        val mockIntent = Intent(app, MockLocationSaveService::class.java)
-        app.startService(mockIntent)
+        val startMockIntent = Intent(app, MockLocationSaveService::class.java)
+        app.startService(startMockIntent)
     }
 
     fun stopMockLocationService(app: Application){
         Log.d(TAG, "stopMockLocationService : Stop mock location service")
-        val mockIntent = Intent(app, MockLocationSaveService::class.java)
-        app.stopService(mockIntent)
+        val stopMockIntent = Intent(app, MockLocationSaveService::class.java)
+        app.stopService(stopMockIntent)
     }
 }
