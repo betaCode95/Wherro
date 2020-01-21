267c576a0e8502a06d04e5440ec97c33c727b129
kshama <kshama.singh@shuttl.com>
2020-01-21 18:14:59 +0530

    Modify onLocationChanged for mockLocation

--- locationsyncservice/src/main/java/com/shuttl/location_pings/service/MockLocationSaveService.kt
+++ locationsyncservice/src/main/java/com/shuttl/location_pings/service/MockLocationSaveService.kt
@@ -1,5 +1,6 @@
 package com.shuttl.location_pings.service
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -10,6 +11,7 @@ import android.os.Binder
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
+import com.shuttl.location_pings.config.components.LocationConfigs
 import com.shuttl.location_pings.config.components.LocationsDB
 import com.shuttl.location_pings.custom.notification
 import com.shuttl.location_pings.data.model.entity.GPSLocation
@@ -20,13 +22,18 @@ class MockLocationSaveService : Service() {
 
     private val TAG: String = "MockLocation"
     private val locManager by lazy { applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager }
+    private var configs: LocationConfigs = LocationConfigs()
     private val repo by lazy { LocationRepo(LocationsDB.create(applicationContext)?.locationsDao()) }
+    val mMockLocationProvider = MockLocationProvider()
     private val mockLocationListener by lazy {
         object : LocationListener {
 
             override fun onLocationChanged(location: Location?) {
-                Log.d(TAG, "onLocationChanged : Update changed location in DB ${location.toString()}")
-                saveMockLocationInDB(location)
+                if (location != null) {
+                    mMockLocationProvider.setMockProviderLocationData(LocationManager.GPS_PROVIDER, applicationContext)
+                    saveMockLocationInDB(location)
+                    Log.d(TAG, "onLocationChanged : Update changed location in DB ${location.toString()}")
+                }
             }
 
             override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
@@ -58,12 +65,13 @@ class MockLocationSaveService : Service() {
         return START_STICKY
     }
 
+    @SuppressLint("MissingPermission")
     private fun work() {
-        val mMockLocationProvider = MockLocationProvider(applicationContext)
         mMockLocationProvider.addMockLocationProvider(
             locManager,
             applicationContext,
-            mockLocationListener
+            mockLocationListener,
+            configs
         )
         Log.d(TAG, "work : Start mock location provider ")
     }
