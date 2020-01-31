cd20b3d4fb168d5b35c806024f7b47ccd42e2c4f
Nikhil Roy <nikhil.roy@shuttl.com>
2020-01-31 15:21:47 +0530

    [WIP] minor name changes

--- locationsyncservice/src/main/java/com/shuttl/location_pings/data/model/entity/GPSLocation.kt
+++ locationsyncservice/src/main/java/com/shuttl/location_pings/data/model/entity/GPSLocation.kt
@@ -7,8 +7,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "gps_locations")
 data class GPSLocation(
-                       @ColumnInfo(name = "lat") val latitude: Double = 0.0,
-                       @ColumnInfo(name = "long") val longitude: Double = 0.0,
+                       @ColumnInfo(name = "latitude") val latitude: Double = 0.0,
+                       @ColumnInfo(name = "longitude") val longitude: Double = 0.0,
                        @ColumnInfo(name = "accuracy") val accuracy: Float = 0.0f,
                        @ColumnInfo(name = "provider") val provider: String = "",
                        @ColumnInfo(name = "user_id") val user_id: String = "",
